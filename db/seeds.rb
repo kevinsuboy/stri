5a0f316e1728800066f3e962f22c458e4e31da29
kevinsu <kevin.suboy@gmail.com>
2020-10-13 16:32:41 -0700

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,39 +10,6 @@ User.delete_all
 Route.delete_all
 Activity.delete_all
 
-r1 = Route.create!(
-    name: 'Morning Jog',
-    description: 'Very easy',
-    distance: 100.00,
-    user_id: 1
-)
-r2 = Route.create!(
-    name: 'Afternoon Bike',
-    description: 'Very hard',
-    distance: 200.00,
-    user_id: 2
-)
-a1 = Activity.create!(
-    sport: 'Running',
-    title: 'Semptember 7 Jog',
-    description: "finished it 10 minutes faster",
-    distance: 120.00,
-    date: Date.new(2000,9,07),
-    duration: "00:56",
-    user_id: 2,
-    route_id: 1
-)
-a2 = Activity.create!(
-    sport: 'Running',
-    title: 'October 31 Bike',
-    description: "finished it 10 minutes slower",
-    distance: 420.00,
-    date: Date.new(2020,10,31),
-    duration: "10:34",
-    user_id: 1,
-    route_id: 2
-)
-
 u1 = User.create!(
     username: "Usain Bolt",
     email: "irunfast@goldmedal.com",
@@ -53,3 +20,36 @@ u2 = User.create!(
     email: "iluvweed@swimslikedolphin.com",
     password: "iamphelps"
 )
+r1 = Route.create!(
+    name: 'Morning Jog',
+    description: 'Very easy',
+    distance: 100.00,
+    user_id: u1.id
+)
+r2 = Route.create!(
+    name: 'Afternoon Bike',
+    description: 'Very hard',
+    distance: 200.00,
+    user_id: u2.id
+)
+a1 = Activity.create!(
+    sport: 'Run',
+    title: 'Semptember 7 Jog',
+    description: "finished it 10 minutes faster",
+    distance: 120.00,
+    date: Date.new(2000,9,07),
+    duration: "00:56",
+    user_id: u2.id,
+    route_id: r1.id
+)
+a2 = Activity.create!(
+    sport: 'Run',
+    title: 'October 31 Bike',
+    description: "finished it 10 minutes slower",
+    distance: 420.00,
+    date: Date.new(2020,10,31),
+    duration: "10:34",
+    user_id: u1.id,
+    route_id: u2.id
+)
+
