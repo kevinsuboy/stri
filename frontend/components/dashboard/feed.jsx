5b0d5e45f22aea56a15ed2df9c1ce2899063610d
kevinsu <kevin.suboy@gmail.com>
2020-10-19 15:28:19 -0700

    feed menu working

--- frontend/components/dashboard/feed.jsx
+++ frontend/components/dashboard/feed.jsx
@@ -5,11 +5,32 @@ import Routes from './routes_feed'
 class Feed extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            showFeed: null
+        }
+        this.toggleFeed = this.toggleFeed.bind(this)
+    }
+    toggleFeed(e){
+        debugger
+        if(!this.state.showFeed){
+            this.setState({ showFeed: "feed-show" })
+        }else{
+            this.setState({ showFeed: null })
+        }
     }
     render() {
+        // debugger
         return (
             <div className="dashboard-feed container">
                 {/* <h1>I am the feed</h1> */}
+                <ul className="feed-group nav-group">
+                    <li className={`feed-drop ${this.state.showFeed ? this.state.showFeed : ""}`} onClick={this.toggleFeed}>Feed<i className="fas fa-angle-down"></i>
+                        <ul className="feed-drop-content dropdown-content container">
+                            <li><p>Activity Feed</p></li>
+                            <li><p>Route Feed</p></li>
+                        </ul>
+                    </li>
+                </ul>
                 <Activities />
                 <Routes />
             </div>
--- frontend/components/dashboard/profile.jsx
+++ frontend/components/dashboard/profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SportLogo from './sport_logo'
+import { Link } from 'react-router-dom'
 
 export default (props) => {
     // const inits = username.split(" ").map((word) => word[0]).join("");
     const {username,n_routes,n_activities,latest} = props;
-    debugger
+    // debugger
     return (
         <div className="dashboard-profile">
         <div className="profile-container">
@@ -29,7 +30,7 @@ export default (props) => {
                 <p className="latest-header">Latest Activity</p>
                 <div className="latest-body">
                     <SportLogo sport={latest.sport} />
-                    <Link to="api/"><span>{latest.title}</span> • {latest.date}</Link>
+                    <Link to="" className={`latest-link`}><span>{latest.title}</span> • {latest.date}</Link>
                 </div>
 
             </div>
