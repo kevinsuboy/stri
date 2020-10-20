dacc0a48eff8cf75272ee69a9a7edc79fcdc3585
kevinsu <kevin.suboy@gmail.com>
2020-10-20 09:22:47 -0700

    heroku checkin

--- frontend/components/dashboard/profile.jsx
+++ frontend/components/dashboard/profile.jsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom'
 export default (props) => {
     // const inits = username.split(" ").map((word) => word[0]).join("");
     const {username,n_routes,n_activities,latest} = props;
-    debugger
+    // debugger
     const latest_el = latest ? 
         <div className="latest-body">
             <SportLogo sport={latest.sport} />
             <Link to="" className={`latest-link`}><span>{latest.title}</span> • {latest.date}</Link>
-        </div> : null;
+        </div> : <div className="latest-null">
+            <SportLogo sport={""} />
+            <span>No Activity to Show</span>
+        </div>;
     return (
         <div className="dashboard-profile">
         <div className="profile-container">
