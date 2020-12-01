5de3146fb5f15ac976f2270c72c84d5857c26f74
kevinsu <kevin.suboy@gmail.com>
2020-11-30 17:29:02 -0800

    prep for new routes act

--- frontend/components/index/activities.jsx
+++ frontend/components/index/activities.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FilterForm from '../search/filter_form'
 import ActivitiesTable from './activities_table'
+import { Link } from 'react-router-dom';
 
 class Activities extends React.Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class Activities extends React.Component {
             <div id="global-index">
             <div className="index-container container">
                 <h1>My Activities</h1>
+                <Link to={`activities/new`} className={`new-activity-link`}>Create a New Activity</Link>
                 <ul className="index-list">
                     <FilterForm sport={true} userId={this.props.userId} submitFilter={this.props.fetchFilteredUserActivities} changeFilter={this.props.changeActivitiesFilter}/>
                     <ActivitiesTable {...this.props}/>
